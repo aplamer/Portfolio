c374f210030589e849c2f36bc767a47705dc5e1e
ALPELA <aplam@buffalo.edu>
2020-09-09 23:40:23 -0400

    Updates

---
+++ precache-manifest.f0e4100ed945f18c785c48caf67cf15d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9cb6f19c94e23662ea67e0b3cde8d3a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "28263e95f74d60a529e6",
+    "url": "/Portfolio/static/css/main.dc68cc5b.chunk.css"
+  },
+  {
+    "revision": "c944e927d79a1ba7c363",
+    "url": "/Portfolio/static/js/2.659bde8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.659bde8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28263e95f74d60a529e6",
+    "url": "/Portfolio/static/js/main.9e3b8d55.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "226b86c01ef6b5dd0c10691963dc5fa2",
+    "url": "/Portfolio/static/media/Angular.226b86c0.png"
+  },
+  {
+    "revision": "12676444510977e5a59e9d73a7dcd776",
+    "url": "/Portfolio/static/media/Artificer.12676444.png"
+  },
+  {
+    "revision": "31495eaffa9512eec0550d81cff7f972",
+    "url": "/Portfolio/static/media/Background+buttons.31495eaf.png"
+  },
+  {
+    "revision": "fa8c91ebe5a902359a99805d3ea9f85c",
+    "url": "/Portfolio/static/media/FrontEnd.fa8c91eb.png"
+  },
+  {
+    "revision": "1372040f093739d181ceac45756d8d3f",
+    "url": "/Portfolio/static/media/Knight.1372040f.png"
+  },
+  {
+    "revision": "a4564c1d146e24d7e056e12085a58f4f",
+    "url": "/Portfolio/static/media/Portfolio.a4564c1d.PNG"
+  },
+  {
+    "revision": "455c722c30eff8e672e3c9bb01851286",
+    "url": "/Portfolio/static/media/Propaganda.455c722c.png"
+  },
+  {
+    "revision": "864eca6e39f6e57e0b0266e43b5ca4cf",
+    "url": "/Portfolio/static/media/React.864eca6e.png"
+  },
+  {
+    "revision": "9fb3c34c63c82fac3ad0ab215d9eb53b",
+    "url": "/Portfolio/static/media/Rebel.9fb3c34c.png"
+  },
+  {
+    "revision": "09967467067fabee9b9faa6ba528d992",
+    "url": "/Portfolio/static/media/collegeImage.09967467.png"
+  },
+  {
+    "revision": "be308b34a8b8b79ddf2739a3e3437c34",
+    "url": "/Portfolio/static/media/github_icon.be308b34.png"
+  },
+  {
+    "revision": "d32c258b823fb942026a1f2250c3b847",
+    "url": "/Portfolio/static/media/linkedin_icon.d32c258b.png"
+  },
+  {
+    "revision": "8c461d9e80339307b47d73bce24c2a62",
+    "url": "/Portfolio/static/media/nyc.8c461d9e.png"
+  },
+  {
+    "revision": "19401f8c3c2fe66f92639f13bcba8872",
+    "url": "/Portfolio/static/media/profile.19401f8c.png"
+  }
+]);
