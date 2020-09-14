30dbfe186ea0d70727c450f3f3b97fe5d8c3b37c
ALPELA <aplam@buffalo.edu>
2020-09-13 20:05:55 -0400

    Added skills section for the education page
    Added a skills section for the education page, assets/data for that section, along with revisions to the college section to make it flexible for different screen sizes.

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,7 +2,7 @@ import React from "react";
 import collegeImage from "../assets/collegeImage.png";
 import OFFLINECLASSES from "../data/classes";
 import ONLINECLASSES from "../data/onlineclasses";
-
+import SKILLS from "../data/skills";
 const OfflineClass = props =>{
     const {title} = props.offlineclass;
 
@@ -17,18 +17,34 @@ const OnlineClass = props =>{
     const {title, image, link} = props.onlineclass;
 
     return (
-        <div style = {{textAlign : "center", color: "white", display:"inline-block", paddingRight: "50px"}}>
+        <div className = "educationOther">
             <h5>{title}</h5>
-            <a href = {link}><img src = {image} className = "onlineClassImg"/></a>
+            <a href = {link}><img src = {image} style = {{height: "160px"}}/></a>
         </div>
     )
 }
+
+const Skill = props =>{
+    const {title, image} = props.skill;
+
+    return (
+        <div className = "educationOther">
+            <h5>{title}</h5>
+            <img src = {image} style = {{height: "120px"}}/>
+        </div>
+    )
+}
+
 const Education = () => (
     <div>
         <div className = "home">
             <h2>College Education</h2>
-            <img id = "collegeImage" src = {collegeImage}/>
-            <img id = "collegeImage" style = {{marginTop: "400px"}} src = {collegeImage}/>
+
+            <div id = "collegeImageContainer">
+            <img className = "collegeImage" src = {collegeImage}/>
+            <img className = "collegeImage" src = {collegeImage}/>
+            </div>
+
             <div className="homeText collegeText">
                 Institution: (SUNY) University at Buffalo
                 <br></br>
@@ -50,7 +66,7 @@ const Education = () => (
         </div>
 
         <div className = "home" style ={{textAlign: "center"}}>
-            <h2 style = {{float: "left", display: "inline-block", display: "absolute", marginTop: "80px"}}>OnlineCourses(Udemy)</h2>
+            <h2 className = "leftTitle" style = {{marginTop: "80px"}}>Online Courses(Udemy):</h2>
             {
                         ONLINECLASSES.map(ONLINECLASS => (
                                 <OnlineClass key={ONLINECLASS.id} onlineclass={ONLINECLASS} />
@@ -58,6 +74,17 @@ const Education = () => (
                         ))
                     }
         </div>
+
+        <div className = "home" style ={{textAlign: "center"}}>
+            <h2 className = "leftTitle" style = {{marginTop: "130px"}}>Skills:</h2>
+            {
+                        SKILLS.map(SKILL => (
+                                <Skill key={SKILL.id} skill={SKILL} />
+                            
+                        ))
+                    }
+        </div>
+
     </div>
 )
 
---
+++ src/data/skills.js
@@ -0,0 +1,82 @@
+import image1 from "../assets/Javascript.png";
+import image2 from "../assets/HTML.png";
+import image3 from "../assets/CSS.png";
+import image4 from "../assets/Reacticon.png";
+import image5 from "../assets/Angularicon.png";
+import image6 from "../assets/Jquery.png";
+import image7 from "../assets/NodeJS.png";
+import image8 from "../assets/Bootstrap.png";
+import image9 from "../assets/Java.png";
+import image10 from "../assets/Python.png";
+import image11 from "../assets/C++.png";
+import image12 from "../assets/SQL.png";
+import image13 from "../assets/CSP.png";
+
+const SKILLS = [
+    {
+        id: 0,
+        title: "JavaScript",
+        image: image1
+    },
+    {
+        id: 1,
+        title: "HTML",
+        image: image2
+    }, 
+    {
+        id: 2,
+        title: "CSS",
+        image: image3
+    },
+    {
+        id: 3,
+        title: "React JS",
+        image: image4
+    },
+    {
+        id: 4,
+        title: "Angular 10",
+        image: image5
+    }, 
+    {
+        id: 5,
+        title: "Jquery",
+        image: image6
+    },
+    {
+        id: 6,
+        title: "Nodejs",
+        image: image7
+    },
+    {
+        id: 7,
+        title: "Bootstrap",
+        image: image8
+    }, 
+    {
+        id: 8,
+        title: "Java",
+        image: image9
+    },
+    {
+        id: 9,
+        title: "Python",
+        image: image10
+    },
+    {
+        id: 10,
+        title: "C++",
+        image: image11
+    }, 
+    {
+        id: 11,
+        title: "SQL",
+        image: image12
+    },
+    {
+        id: 12,
+        title: "CLIP STUDIO PAINT",
+        image: image13
+    },
+]
+export default SKILLS;
