4b3eb03cf58b0d732e12f5f3ea9e909a899b5ba2
aplamer <aplamer11307@buffalo.edu>
2020-10-18 12:11:16 -0400

    Updated project banner
    Updated project banner with new website.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,34 +1,19 @@
 import project1 from "../assets/Portfolio.PNG"
+import project2 from "../assets/ColorTheory.PNG"
 const PROJECTS = [
     {
         id: 1,
         title: 'Portfolio Website',
         description: 'The website you\'re on right now! Created to showcase myself to potential employer. This website does it\'s best to showcase a firm understanding of web development. Technologies used: Javascript, CSS, HTML, React JS, Bootstrap(Styling only)',
-        image: project1
+        image: project1,
+        link: "https://aplamer.github.io/Portfolio/#/"
     },
     {
         id: 2,
-        title: 'Portfolio Website',
-        description: 'The website you\'re on right now! Created to showcase myself to potential employer. This website does it\'s best to showcase a firm understanding of web development. Technologies used: Javascript, CSS, HTML, React JS, Bootstrap(Styling only)',
-        image: project1
-    },
-    {
-        id: 3,
-        title: 'Portfolio Website',
-        description: 'The website you\'re on right now! Created to showcase myself to potential employer. This website does it\'s best to showcase a firm understanding of web development. Technologies used: Javascript, CSS, HTML, React JS, Bootstrap(Styling only)',
-        image: project1
-    },
-    {
-        id: 4,
-        title: 'Portfolio Website',
-        description: 'The website you\'re on right now! Created to showcase myself to potential employer. This website does it\'s best to showcase a firm understanding of web development. Technologies used: Javascript, CSS, HTML, React JS, Bootstrap(Styling only)',
-        image: project1
-    },
-    {
-        id: 5,
-        title: 'Portfolio Website',
-        description: 'The website you\'re on right now! Created to showcase myself to potential employer. This website does it\'s best to showcase a firm understanding of web development. Technologies used: Javascript, CSS, HTML, React JS, Bootstrap(Styling only)',
-        image: project1
+        title: 'Color Theory Website',
+        description: 'A website for informing people about the different aspects of color theory, with some tools to help understand and utilize them better. Technologies used: CSS, HTML, Angular 10, Bootstrap(Styling only), Typescript',
+        image: project2,
+        link: "https://aplamer.github.io/ColorTheory/about"
     }
     
 ];
